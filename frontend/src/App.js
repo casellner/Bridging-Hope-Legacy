0074e54c5552a89e5fec9231098f990312c59191
casellner <114782256+casellner@users.noreply.github.com>
2024-09-03 15:47:40 -0500

    Change App.js to contain landing page informaton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo from './BridgingHopeLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <img src={logo}/>
+        
       </header>
+      <body>
+        <h1>Bridging Hope</h1>
+        <p>Our application is a powerful tool for nonprofits. It simplifies the process of tracking clients, ensuring that those who need assistance are reached with precision and care.</p>
+      
+        <h2>Key Features</h2>
+        <hr></hr> 
+        <ul>
+          <li>Register Clients</li>
+          <li>Log Visits</li>
+          <li>Search Clients</li>
+        </ul>
+
+        <h2>Contact an Administrator to connect your organization to Bridging Hope</h2>
+        <ul>
+          <li>
+            <p>Mrs. X</p>
+          </li>
+          <li>
+            <p>Mr. Y</p>
+          </li>
+        </ul>
+      </body>
     </div>
   );
 }
