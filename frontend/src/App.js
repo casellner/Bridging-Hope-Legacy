827db3fbcf76872f250c26a65eec8d1310aae8fe
casellner <114782256+casellner@users.noreply.github.com>
2024-09-05 13:08:55 -0500

    add bootstrap to content in landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,26 +19,40 @@ function App() {
       </nav>
 
       <body>
-        <h1 className='text-center'>Bridging Hope</h1>
-        <p>Our application is a powerful tool for nonprofits. It simplifies the process of tracking clients, ensuring that those who need assistance are reached with precision and care.</p>
-      
-        <h2>Key Features</h2>
-        <hr></hr> 
-        <ul>
-          <li>Register Clients</li>
-          <li>Log Visits</li>
-          <li>Search Clients</li>
-        </ul>
+        <div className="card">
+          <div className="card-header">
+            <h1 className='text-center'>Bridging Hope</h1>
+            <p>Our application is a powerful tool for nonprofits. It simplifies the process of tracking clients, ensuring that those who need assistance are reached with precision and care.</p>
+          </div>
 
-        <h2>Contact an Administrator to connect your organization to Bridging Hope</h2>
-        <ul className="list-group list-group-flush">
-          <li>
-            <p>Mrs. X</p>
-          </li>
-          <li>
-            <p>Mr. Y</p>
-          </li>
-        </ul>
+          <div className="card-body">
+            {/* key features */}
+            <div className="bg-info">
+              <h2 className="text-center">Key Features</h2>
+              <hr></hr> 
+              <ul className="list-group list-group-horizontal">
+                <li className="list-group-item">Register Clients</li>
+                <li className="list-group-item">Log Visits</li>
+                <li className="list-group-item">Search Clients</li>
+              </ul>
+            </div>
+
+            {/* contact an admin section */}
+            <h2>Contact an Administrator to connect your organization to Bridging Hope</h2>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <h3>Ms. X</h3>
+                <p>phone: </p>
+                <p>email: </p>
+              </li>
+              <li className="list-group-item">
+                <h4>Mr. Y</h4>
+                <p>phone: </p>
+                <p>email: </p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </body>
     </div>
   );
