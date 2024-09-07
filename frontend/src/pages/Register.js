13ec99ab1314ed93facf5ec5a288e11297a29c70
casellner <114782256+casellner@users.noreply.github.com>
2024-09-07 11:47:28 -0500

    add organization select

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -22,21 +22,32 @@ const Register = () => {
                     </div>
                     
                     <div className="card-body p-0">
-                        {/* Sign in form */}
+                        {/* Registration form */}
                         <div className="bg-info rounded-bottom-1 pb-3">
                             <form className="col-8 offset-2"> { /* these columns could be adjusted for different screen sizes */ }
+                                { /* username and password */ }
                                 <label for="txtUsername" className="form-label mt-2">Username</label>
                                 <input id="txtUsername" type="text" placeholder="johndoe" className="form-control" />
                                 <label for="txtPassword" className="form-label mt-2">Password</label>
                                 <input id="txtPassword" type="password" className="form-control" />
 
+                                { /* first and last name */ }
                                 <label for="txtFirstName" className="form-label mt-5">First Name</label>
                                 <input id="txtFirstName" type="text" placeholder="John" className="form-control" />
                                 <label for="txtLastName" className="form-label mt-2">Last Name</label>
                                 <input id="txtLastName" type="text" placeholder="Doe" className="form-control" />
 
-                                <button id="btnContinue" type="button" class="btn btn-primary mt-5 col-12">Continue</button>
-                                { /* TODO use flex to center this link */ }
+                                { /* organizations */ }
+                                <label for="txtOrganization" className="form-label mt-5">Choose your organization</label>
+                                <select id="txtOrganization" className="form-select" aria-label="select organization">
+                                    <option selected className="text-secondary">select</option>
+                                    { /* TODO populate with organizations from the database */ }
+                                    <option value="1">Organization XYZ</option>
+                                    <option value="2">Organization ABC</option>
+                                </select>
+
+                                <button id="btnRegister" type="button" className="btn btn-primary mt-5 col-12">Register</button>
+                                
                                 <Link to="/signin">
                                     <a className="d-flex justify-content-center mt-3">Sign in instead</a>
                                 </Link>
