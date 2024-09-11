0a31052215ed680edd78f07def8fd794307c202a
casellner <114782256+casellner@users.noreply.github.com>
2024-09-11 10:20:36 -0500

    Add content to Register Client and Organization

--- frontend/src/content/Organization.js
+++ frontend/src/content/Organization.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Organization() {
-    return (
-      <React.Fragment>
-        <h1>TODO: make organization</h1>
-      </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className="card col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-1 offset-sm-0"> {/* this card will have different widths depending on the resolution of the device */}
+        <div className="card-body">
+          <h2 className="text-center">Currently signed into: Organization XYZ</h2>
+          <button type="button" className="btn btn-primary mt-2">Switch Organization</button>
+          <button type="button" className="btn btn-warning mt-2">Edit Organization</button>
+          <button type="button" className="btn btn-primary mt-2">Manage Volunteers</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Organization;
