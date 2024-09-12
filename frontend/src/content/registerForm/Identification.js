83c8fbfe6123ba7816c46257f7507eb5d6136107
casellner <114782256+casellner@users.noreply.github.com>
2024-09-12 15:35:13 -0500

    Add inputs for Identification

--- frontend/src/content/registerForm/Identification.js
+++ frontend/src/content/registerForm/Identification.js
@@ -5,7 +5,20 @@ function BasicInfo() {
         <React.Fragment>
             <label for="txtDateOfBirth" className="form-label mt-2">Date of birth</label>
             <input id="txtDateOfBirth" type="date" className="form-control" />
-            <h1>TODO add other fields</h1>
+            <label for="txtPhone" className="form-label mt-2">Phone number</label>
+            <input id="txtPhone" type="tel" className="form-control" />
+            <label for="imgProfilePic" className="form-label mt-2">Profile picture</label>
+            <input id="imgProfilePic" class="form-control" type="file" />
+            
+            { /* ID section. This may need to be changed drasticallys */}
+            <label for="txtIDType" className="form-label mt-2">ID type</label>
+            <select id="txtIDType" className="form-select">
+            <option selected className="text-secondary" aria-label="select ID Type">select</option>
+                <option value="1">Driver's license</option>
+                <option value="2">Other</option>
+            </select>
+            <label for="txtIDNumber" className="form-label mt-2">ID number</label>
+            <input id="txtIDNumber" type="text" className="form-control" />
         </React.Fragment>
     );
 }
