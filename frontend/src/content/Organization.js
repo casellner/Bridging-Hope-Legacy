d03482fa72243f21ca5152a48c4d25ec93067994
casellner <114782256+casellner@users.noreply.github.com>
2024-09-12 15:40:51 -0500

    adjust organization button sizes

--- frontend/src/content/Organization.js
+++ frontend/src/content/Organization.js
@@ -6,9 +6,9 @@ function Organization() {
       <div className="card col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-1 offset-sm-0"> {/* this card will have different widths depending on the resolution of the device */}
         <div className="card-body">
           <h2 className="text-center">Currently signed into: Organization XYZ</h2>
-          <button type="button" className="btn btn-primary mt-2">Switch Organization</button>
-          <button type="button" className="btn btn-warning mt-2">Edit Organization</button>
-          <button type="button" className="btn btn-primary mt-2">Manage Volunteers</button>
+          <button type="button" className="btn btn-primary mt-2 col-6 offset-3">Switch Organization</button>
+          <button type="button" className="btn btn-warning mt-2 col-6 offset-3">Edit Organization</button>
+          <button type="button" className="btn btn-primary mt-2 col-6 offset-3">Manage Volunteers</button>
         </div>
       </div>
     </React.Fragment>
