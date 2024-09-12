7a50d6edb4a276f30d972445dc64cbca3a12f60f
casellner <114782256+casellner@users.noreply.github.com>
2024-09-12 15:48:37 -0500

    add more fields to client search

--- frontend/src/content/SearchClient.js
+++ frontend/src/content/SearchClient.js
@@ -7,16 +7,53 @@ function SearchClient() {
         <div className="card-body p-0">
           {/* Search fields */}
           <form className="col-8 offset-2"> { /* these columns could be adjusted for different screen sizes */}
-            <label for="txtFirstName" className="form-label mt-2">First name</label>
-            <input id="txtFirstName" type="text" placeholder="John" className="form-control" />
-            <label for="txtLastName" className="form-label mt-2">Last name</label>
-            <input id="txtLastName" type="text" placeholder="Doe" className="form-control" />
-            <label for="txtDOB" className="form-label mt-2">Date of Birth</label>
-            <input id="txtDOB" type="date" className="form-control" />
-            <label for="txtPhone" className="form-label mt-2">Phone</label>
-            <input id="txtPhone" type="tel" placeholder="(555) 123-4567" className="form-control" />
+            <div className="row">
+              <div className="col">
+                <label for="txtFirstName" className="form-label mt-2">First name</label>
+                <input id="txtFirstName" type="text" placeholder="John" className="form-control" />
+              </div>
+              <div className="col">
+                <label for="txtLastName" className="form-label mt-2">Last name</label>
+                <input id="txtLastName" type="text" placeholder="Doe" className="form-control" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <label for="txtDOB" className="form-label mt-2">Date of Birth</label>
+                <input id="txtDOB" type="date" className="form-control" />
+              </div>
+              <div className="col">
+                <label for="txtPhone" className="form-label mt-2">Phone</label>
+                <input id="txtPhone" type="tel" placeholder="(555) 123-4567" className="form-control" />
+              </div>
+            </div>
+            
+            { /* ID section. This may need to be changed drastically */}
+            <div className="row">
+              <div className="col">
+                <label for="txtIDType" className="form-label mt-2">ID type</label>
+                <select id="txtIDType" className="form-select">
+                <option selected className="text-secondary" aria-label="select ID Type">select</option>
+                    <option value="1">Driver's license</option>
+                    <option value="2">Other</option>
+                </select>
+              </div>
+              <div className="col">
+                <label for="txtIDNumber" className="form-label mt-2">ID number</label>
+                <input id="txtIDNumber" type="text" className="form-control" />
+              </div>
+            </div>
+
+            { /* Email and Search Button */}
             <label for="txtEmail" className="form-label mt-2">Email</label>
-            <input id="txtEmail" type="email" placeholder="jdoe@email.com" className="form-control" />
+            <div className="row mb-3">
+              <div className="col">
+                <input id="txtEmail" type="email" placeholder="jdoe@email.com" className="form-control" />
+              </div>
+              <div className="col">
+                <button type="button" className="btn btn-success">Search</button>
+              </div>
+            </div>
           </form>
         </div>
       </div>
--- frontend/src/content/registerForm/Identification.js
+++ frontend/src/content/registerForm/Identification.js
@@ -10,7 +10,7 @@ function BasicInfo() {
             <label for="imgProfilePic" className="form-label mt-2">Profile picture</label>
             <input id="imgProfilePic" class="form-control" type="file" />
             
-            { /* ID section. This may need to be changed drasticallys */}
+            { /* ID section. This may need to be changed drastically */}
             <label for="txtIDType" className="form-label mt-2">ID type</label>
             <select id="txtIDType" className="form-select">
             <option selected className="text-secondary" aria-label="select ID Type">select</option>
