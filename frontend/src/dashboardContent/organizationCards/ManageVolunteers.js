ed057d15865d972961d26b7d7486c9c251eb77a3
casellner <114782256+casellner@users.noreply.github.com>
2024-09-13 16:17:59 -0500

    fix Manage Volunteers button

--- frontend/src/dashboardContent/organizationCards/ManageVolunteers.js
+++ frontend/src/dashboardContent/organizationCards/ManageVolunteers.js
@@ -6,6 +6,7 @@ const ManageVolunteers = ({ onSelectBack }) => {
       <div className="card col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-1 offset-sm-0"> {/* this card will have different widths depending on the resolution of the device */}
         <div className="card-body">
           <p>TODO also</p>
+          <p>Manage Volunteers</p>
 
           {/* TODO: change this to match wireframes */}
           <button type="button" className="btn btn-warning mt-2 col-4 offset-4" onClick={onSelectBack}>Go back</button>
--- frontend/src/dashboardContent/organizationCards/OrganizationOptions.js
+++ frontend/src/dashboardContent/organizationCards/OrganizationOptions.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const OrganizationOptions = ({ onSelectSwitchOrg, onSelectEditOrg }) => {
+const OrganizationOptions = ({ onSelectSwitchOrg, onSelectEditOrg, onSelectManageVolunteers }) => {
   return (
     <React.Fragment>
       <div className="card col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-1 offset-sm-0"> {/* this card will have different widths depending on the resolution of the device */}
@@ -10,7 +10,7 @@ const OrganizationOptions = ({ onSelectSwitchOrg, onSelectEditOrg }) => {
         <div className="card-body">
           <button type="button" className="btn btn-primary col-6 offset-3" onClick={onSelectSwitchOrg}>Switch Organization</button>
           <button type="button" className="btn btn-warning mt-2 col-6 offset-3" onClick={onSelectEditOrg}>Edit Organization</button>
-          <button type="button" className="btn btn-primary mt-2 col-6 offset-3">Manage Volunteers</button>
+          <button type="button" className="btn btn-primary mt-2 col-6 offset-3" onClick={onSelectManageVolunteers}>Manage Volunteers</button>
         </div>
       </div>
     </React.Fragment>
