e7bd1dda97dd7f322ae7c4ed37a25ea8b1b0a373
casellner <114782256+casellner@users.noreply.github.com>
2024-09-13 09:48:42 -0500

    fix indentation

--- frontend/src/dashboardContent/organizationCards/OrganizationOptions.js
+++ frontend/src/dashboardContent/organizationCards/OrganizationOptions.js
@@ -5,7 +5,7 @@ const OrganizationOptions = ({ onSelect }) => {
     <React.Fragment>
       <div className="card col-lg-6 col-md-10 col-sm-12 offset-lg-3 offset-md-1 offset-sm-0"> {/* this card will have different widths depending on the resolution of the device */}
         <div className="card-header">
-        <h2 className="text-center">Currently signed into: Organization XYZ</h2>
+          <h2 className="text-center">Currently signed into: Organization XYZ</h2>
         </div>
         <div className="card-body">
           <button type="button" className="btn btn-primary col-6 offset-3" onClick={onSelect}>Switch Organization</button>
