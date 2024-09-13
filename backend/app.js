189e72a767efc469f55c65a61d8aa03c28ddce40
njlamb <142761756+njlamb@users.noreply.github.com>
2024-09-13 11:23:30 -0500

    Created new backend branch

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,47 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var cors = require("cors");
+var mariadb = require("mariadb");
 
+
+/* Need for later
 app.get("/", (req, res) => {
   res.send("I'm inside docker!");
 });
-
+*/
 const port = 3000;
 app.listen(port, () => {
   console.log(`Express listening at http://0.0.0.0:${port}`);
 });
+
+const db_pool = mariadb.createPool({
+	host: "localhost",
+	user: process.env["MARIADB_USER"],
+	password: process.env["MARIADB_PASSWORD"],
+	connectionLimit: 5,
+	database: "",/*INSERT DATA BASE NAME*/
+	port: 3000
+});
+
+var app = express();
+app.use(express.json());
+app.use(cors());
+
+//delete unwanted characters
+function clean(str) {
+	return str.replace(/[^0-9a-zA-Z_\-@.\s]/gi, "");
+}
+
+
+//checking user and password
+app.post("/login", (req, res) => {
+    console.log(req.body);
+
+    const { username, password } = req.body;
+    if (username === 'admin' && password === 'password') {
+      res.json({ message: 'Logged in successfully' });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+});
+
+ 
