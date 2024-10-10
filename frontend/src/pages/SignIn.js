cb26e1b95a419f35a68c83a2ef8c587d1f064f23
casellner <114782256+casellner@users.noreply.github.com>
2024-10-10 14:53:24 -0500

    Change all occurrences of class to className

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -4,35 +4,35 @@ import { Link } from 'react-router-dom';
 const SignIn = () => {
   return (
     <React.Fragment>
-      <div class="col-12 d-flex justify-content-center align-items-center" >
-        <div class="col-11 col-md-4 col-lg-3 card my-4">
-          <div class="card-body">
-            <div class="d-flex col-12 mt-4 justify-content-center align-items-center">
-              <img src="images/BridgingHopeWord.svg" class="img-fluid" alt="logo" style={{ maxWidth: "50%" }} />
+      <div className="col-12 d-flex justify-content-center align-items-center" >
+        <div className="col-11 col-md-4 col-lg-3 card my-4">
+          <div className="card-body">
+            <div className="d-flex col-12 mt-4 justify-content-center align-items-center">
+              <img src="images/BridgingHopeWord.svg" className="img-fluid" alt="logo" style={{ maxWidth: "50%" }} />
             </div>
-            <form class="mt-5">
-              <label class="form-label">Email</label>
-              <input type="email" class="form-control mb-3" placeholder="Email" />
-              <label class="form-label mt-3">Password</label>
-              <input type="password" class="form-control mb-3" placeholder="Password" />
-              <button type="button" class="btn btn-primary col-12 mt-3">Login</button>
-              <Link to="/register" type="button" class="btn btn-secondary col-12 mt-2">Register</Link>
+            <form className="mt-5">
+              <label className="form-label">Email</label>
+              <input type="email" className="form-control mb-3" placeholder="Email" />
+              <label className="form-label mt-3">Password</label>
+              <input type="password" className="form-control mb-3" placeholder="Password" />
+              <button type="button" className="btn btn-primary col-12 mt-3">Login</button>
+              <Link to="/register" type="button" className="btn btn-secondary col-12 mt-2">Register</Link>
               <hr />
-              <div class="col-12 d-flex justify-content-center align-items-center mb-3">
-                <a href="" class="text-center col-12">Forgot Password</a>
+              <div className="col-12 d-flex justify-content-center align-items-center mb-3">
+                <a href="" className="text-center col-12">Forgot Password</a>
               </div>
             </form>
           </div>
         </div>
       </div>
 
-      <footer class="footer">
-        <div class="footer-body d-flex justify-content-between mx-4 pb-3">
-          <ul class="list-inline mb-0 p-0">
-            <li class="list-inline-item"><a href="./dashboard/extra/privacy-policy.html">Privacy Policy</a></li>
-            <li class="list-inline-item"><a href="./dashboard/extra/terms-of-service.html">Terms of Use</a></li>
+      <footer className="footer">
+        <div className="footer-body d-flex justify-content-between mx-4 pb-3">
+          <ul className="list-inline mb-0 p-0">
+            <li className="list-inline-item"><a href="./dashboard/extra/privacy-policy.html">Privacy Policy</a></li>
+            <li className="list-inline-item"><a href="./dashboard/extra/terms-of-service.html">Terms of Use</a></li>
           </ul>
-          <div class="right-panel">
+          <div className="right-panel">
             ©<script>document.write(new Date().getFullYear())</script> Bridging Hope. All Rights Reserved.
           </div>
         </div>
