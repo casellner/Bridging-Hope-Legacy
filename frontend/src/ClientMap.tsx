2e1ef1214a64d5a5465f74573574c5f2c362398f
casellner <114782256+casellner@users.noreply.github.com>
2024-11-13 10:14:38 -0600

    remove unused code

--- frontend/src/ClientMap.tsx
+++ frontend/src/ClientMap.tsx
@@ -21,17 +21,7 @@ const data = getData()
 const Z_INDEX_SELECTED = data.length;
 const Z_INDEX_HOVER = data.length + 1;
 
-/* let origin = '1 William L Jones Dr, Cookeville, TN 38505';
-let destination = ''; */
-
 const ClientMap = () => {
-  /* const [directionsVisible, setDirectionsVisible] = useState(false);
-
-  function handleDirectionsClick(address: string) {
-    destination = address;
-    setDirectionsVisible(true);
-  } */
-
   const [markers] = useState(data);
 
   const [hoverId, setHoverId] = useState<string | null>(null);
@@ -121,13 +111,10 @@ const ClientMap = () => {
             pixelOffset={[0, -2]}
             onCloseClick={handleInfowindowCloseClick}>
             <h2>{markers[Number(selectedId)].name}</h2>
-            {/* <button type='button' className='btn btn-primary my-2' onClick={() => handleDirectionsClick(markers[Number(selectedId)].address)}>Directions</button> */}
             <a href={`https://google.com/maps/search/${markers[Number(selectedId)].name}`}>See on Google Maps</a>
             <p>{markers[Number(selectedId)].description}</p>
           </InfoWindow>
         )}
-
-        {/* { directionsVisible ? <Directions /> : null } */}
       </Map>
     </APIProvider>
   );
@@ -204,74 +191,5 @@ function getData() {
 
   return data;
 }
-
-/* function Directions() {
-  const map = useMap();
-  const routesLibrary = useMapsLibrary('routes');
-  const [directionsService, setDirectionsService] =
-    useState<google.maps.DirectionsService>();
-  const [directionsRenderer, setDirectionsRenderer] =
-    useState<google.maps.DirectionsRenderer>();
-  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
-  const [routeIndex, setRouteIndex] = useState(0);
-  const selected = routes[routeIndex];
-  const leg = selected?.legs[0];
-
-  // Initialize directions service and renderer
-  useEffect(() => {
-    if (!routesLibrary || !map) return;
-    setDirectionsService(new routesLibrary.DirectionsService());
-    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({map}));
-  }, [routesLibrary, map]);
-
-  // Use directions service
-  useEffect(() => {
-    if (!directionsService || !directionsRenderer) return;
-
-    directionsService
-      .route({
-        origin: origin,
-        destination: destination,
-        travelMode: google.maps.TravelMode.DRIVING,
-        provideRouteAlternatives: true
-      })
-      .then(response => {
-        directionsRenderer.setDirections(response);
-        setRoutes(response.routes);
-      });
-
-    return () => directionsRenderer.setMap(null);
-  }, [directionsService, directionsRenderer]);
-
-  // Update direction route
-  useEffect(() => {
-    if (!directionsRenderer) return;
-    directionsRenderer.setRouteIndex(routeIndex);
-  }, [routeIndex, directionsRenderer]);
-
-  if (!leg) return null;
-
-  return (
-    <div className="directions">
-      <h2>{selected.summary}</h2>
-      <p>
-        {leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]}
-      </p>
-      <p>Distance: {leg.distance?.text}</p>
-      <p>Duration: {leg.duration?.text}</p>
-
-      <h2>Other Routes</h2>
-      <ul>
-        {routes.map((route, index) => (
-          <li key={route.summary}>
-            <button onClick={() => setRouteIndex(index)}>
-              {route.summary}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-} */
 
 export default ClientMap;
