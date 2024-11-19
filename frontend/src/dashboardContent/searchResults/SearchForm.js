1b9ba6643f904990aac4453b62eb0a79df0ed8fd
casellner <114782256+casellner@users.noreply.github.com>
2024-11-19 11:41:28 -0600

    Fix inputs not working by changing ids

--- frontend/src/dashboardContent/searchResults/SearchForm.js
+++ frontend/src/dashboardContent/searchResults/SearchForm.js
@@ -31,46 +31,46 @@ function SearchForm({ onSearch }) {
       <form className="pb-3 rounded"> { /* these columns could be adjusted for different screen sizes */}
         <div className="row mx-3">
           <div className="col">
-            <label for="txtFirstName" className="form-label mt-2">First name</label>
-            <input id="txtFirstName" type="text" placeholder="John" className="form-control" value={formData.firstName} onChange={handleChange} />
+            <label for="firstName" className="form-label mt-2">First name</label>
+            <input id="firstName" type="text" placeholder="John" className="form-control" value={formData.firstName} onChange={handleChange} />
           </div>
           <div className="col">
-            <label for="txtLastName" className="form-label mt-2">Last name</label>
-            <input id="txtLastName" type="text" placeholder="Doe" className="form-control" value={formData.lastName} onChange={handleChange} />
+            <label for="lastName" className="form-label mt-2">Last name</label>
+            <input id="lastName" type="text" placeholder="Doe" className="form-control" value={formData.lastName} onChange={handleChange} />
           </div>
         </div>
         <div className="row mx-3">
           <div className="col">
-            <label for="txtDOB" className="form-label mt-2">Date of Birth</label>
-            <input id="txtDOB" type="date" className="form-control" value={formData.DOB} onChange={handleChange} />
+            <label for="DOB" className="form-label mt-2">Date of Birth</label>
+            <input id="DOB" type="date" className="form-control" value={formData.DOB} onChange={handleChange} />
           </div>
           <div className="col">
-            <label for="txtPhone" className="form-label mt-2">Phone</label>
-            <input id="txtPhone" type="tel" placeholder="(555) 123-4567" className="form-control" value={formData.phone} onChange={handleChange} />
+            <label for="phone" className="form-label mt-2">Phone</label>
+            <input id="phone" type="tel" placeholder="(555) 123-4567" className="form-control" value={formData.phone} onChange={handleChange} />
           </div>
         </div>
 
         { /* ID section. This may need to be changed drastically */}
         <div className="row mx-3">
           <div className="col">
-            <label for="txtIDType" className="form-label mt-2">ID type</label>
-            <select id="txtIDType" className="form-select">
+            <label for="IDType" className="form-label mt-2">ID type</label>
+            <select id="IDType" className="form-select">
               <option selected className="text-secondary" aria-label="select ID Type">select</option>
               <option value="1">Driver's license</option>
               <option value="2">Other</option>
             </select>
           </div>
           <div className="col">
-            <label for="txtIDNumber" className="form-label mt-2">ID number</label>
-            <input id="txtIDNumber" type="text" className="form-control" />
+            <label for="IDNumber" className="form-label mt-2">ID number</label>
+            <input id="IDNumber" type="text" className="form-control" />
           </div>
         </div>
 
         { /* Email and Search Button */}
         <div className="row mb-3 mx-3">
           <div className="col">
-            <label for="txtEmail" className="form-label mt-2">Email</label>
-            <input id="txtEmail" type="email" placeholder="jdoe@email.com" className="form-control" value={formData.email} onChange={handleChange} />
+            <label for="email" className="form-label mt-2">Email</label>
+            <input id="email" type="email" placeholder="jdoe@email.com" className="form-control" value={formData.email} onChange={handleChange} />
           </div>
           <div className="col align-self-end">
             <button type="button" className="btn btn-success" onClick={handleSearchClick}>Search</button>
