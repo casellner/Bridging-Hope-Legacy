72d59d99c852ee03c0ac1d56285518cfaec00343
zepz001 <159800345+zepz001@users.noreply.github.com>
2024-12-06 09:33:09 -0600

    Update Register.js
    deleted a duplicate function and fixed the array holding the values

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -28,19 +28,6 @@ const Register = () => {
     fetchOrganizations();
   }, []);
 
-  useEffect(() => { //Code that gets the list of organizations from the backend to ppopulate the organization field
-    const fetchOrganizations = async () => {
-      try {
-        const response = await axios.get('https://bridginghope.life/api/get_org_names');
-        //const response = await axios.get('http://localhost:4433/api/get_org_names'); //uncomment for local testing
-        setOrganizations(response.data);
-      } catch (err) {
-        console.error('Error fetching organizations:', err);
-      }
-    };
-
-    fetchOrganizations();
-  }, []);
 
   const url = 'https://bridginghope.life/api/register'; 
   //const url = 'http://localhost:4433/api/register';  //uncomment for local testing
@@ -49,8 +36,6 @@ const Register = () => {
 
   const [formData, setFormData] = useState({
       username: '',
-      email: '',
-
       email: '',
       password: '',
       confirmPassword: '',
